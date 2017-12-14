f8c60d9e69c0f0fa3163b7a9fbe20c8f8e6f6575
giggly <giggly@abv.bg>
2017-12-14 23:48:43 +0200

    The 'Try It Out' section with additional challenges

---
+++ 04. Try It Out!/04. Try It Out!/02. Reverse It!/02. Reverse It!.cs
@@ -0,0 +1,35 @@
+﻿/*
+Reverse It!
+In this task, we’ re going to make a simple array-based game. We’ll start by making an array that can 
+hold 9 integer values, and we’ll randomly put the numbers 1 through 9 in the array. Make sure you 
+don’t have any duplicates. Each number should appear exactly once.
+The array will be printed out to the user like this:
+5 3 8 6 4 1 2 9 7
+Allow the user to type in a number 1 through 9. Whatever the user types in, we’ re going to reverse 
+that many numbers, starting at the front. So from the randomized starting position shown above, if 
+the user typed in the number 4, we’ d end up reversing the first four numbers, and the resulting array 
+would be:
+6 8 3 5 4 1 2 9 7
+The player can keep typing in numbers, and you will keep reversing the indicated part of the array 
+until the array is in ascending order:
+1 2 3 4 5 6 7 8 9
+When this happens, the player has won the game, and the game ends. Before closing, show the 
+number of moves it took the player to win.
+Can you figure out an optimal strategy for this game? It is possible to win this game in no more than 
+16 moves, regardless of the starting arrangement.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Reverse_It_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 04. Try It Out!/04. Try It Out!/04. Try It Out!/01. Message from Julius Caesar.cs
@@ -0,0 +1,78 @@
+﻿/*
+Message from Julius Caesar
+Encryption is the process of taking data and turning it into a form that is no longer readable . This 
+keeps the information protected, so that people who aren’t supposed to read it can’t. Of course, the 
+person who is supposed to read it needs to be able to decrypt it and recover the message.
+When encryption is done, the algorithm used to encrypt stuff usually uses a special value called a 
+key to perform the encryption. If a person has the key, they can usually decrypt the message as well.
+The Caesar cipher is one of the simplest encryption schemes, and it is possibly something you used 
+to send coded messages to friends when you were younger. It is an encryption method that
+supposedly Julius Caesar occasionally used when he wrote personal letters.
+The basic idea is that for every letter in your message, you shift it down the alphabet by a certain 
+number of letters. The amount you shift is the key for the algorithm. If you are using a key of 3, A 
+would become D, B would become E, etc. Once you get to the end of the alphabet, it wraps back 
+around, so Z would be C.
+For example, with a key of 4, the message below is encrypted to look like this:
+Plain text: EXPERIENCE IS THE TEACHER OF ALL THINGS
+Encrypted:  IBTIVMIRGI MW XLI XIEGLIV SJ EPP XLMRKW
+Write a program that will read in a message from a file (or from the user) and encrypt it, writing out 
+the encrypted message to another file (or back to the console window). Don’t overwrite the original 
+file. Ideally, your program will ask for the name of a file to read, and a key (a number) to use in the 
+encryption.
+Anything besides a letter (punctuation, numbers, etc.) can either be skipped or passed along to the 
+output without encrypting it.
+Also create code that will do the reverse, decrypting a message given a decryption key.
+If you want an extra challenge, try this. The Caesar cipher is really easy to crack. In fact, it is so basic 
+that it provides little real protection in modern usage. Much more sophisticated algorithms are used 
+now to encrypt data. To prove the point that the Caesar cipher can be cracked, we’ re going to try a 
+“brute force” approach to crack the following code:
+UX LNKX MH WKBGD RHNK HOTEMBGX
+With the Caesar cipher, there are only 26 possible keys. Try each one, one at a time, until the 
+decrypted message makes sense. The simple approach for this is to have a human (you) visually 
+inspect the decrypted message to see if it makes sense. While it is more work, it is possible to have 
+the computer figure out if the message has been decrypted by using a dictionary file (containing all 
+or the words in the English language) and checking to see if all or most of the decrypted words are in 
+it. A high percentage typically indicates a successful decryption.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Try_It_Out_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter message to be encrypted:");
+            string encoded = Console.ReadLine();
+            Console.WriteLine("Enter key:");
+            int key = int.Parse(Console.ReadLine());
+            StringBuilder decoded = new StringBuilder();
+
+            for (int i = 0; i < encoded.Length; i++)
+            {
+                if (char.IsLetter(encoded[i]))
+                {
+                    if (Convert.ToInt32(encoded[i]) + key > 90)
+                    {
+                        decoded.Append(Convert.ToChar(Convert.ToInt32(encoded[i]) + key - 90 + 64));
+                    } else
+                    {
+                        decoded.Append(Convert.ToChar(Convert.ToInt32(encoded[i]) + key));
+                    }
+                }
+                else
+                {
+                    decoded.Append(encoded[i]);
+                }
+            }
+
+            Console.WriteLine("Plain text: " + encoded);
+            Console.WriteLine("Encrypted: " + decoded.ToString());
+
+        }
+    }
+}
